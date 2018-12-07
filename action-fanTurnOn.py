496818b5b5106686624526de72aa4bf89744699a
LordTyrion123 <monu_96@yahoo.com>
2018-12-07 11:09:58 +0530

    Update action-fanTurnOn.py

--- action-fanTurnOn.py
+++ action-fanTurnOn.py
@@ -5,6 +5,7 @@ from snipsTools import SnipsConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
+from pixels import pixels
 
 CONFIG_INI = "config.ini"
 
@@ -36,6 +37,11 @@ class fanTurnOn(object):
         client.connect(MQTT_IP_ADDR,1883)     #Ip address and port
         client.publish("inTopic","0")     #gatewayUID
         
+    def lightcommand(self):
+        pixels.think()
+        time.sleep(4)
+        pixels.off()
+        
     # --> Sub callback function, one per intent
     def fanTurnOn_callback(self, hermes, intent_message):
         # terminate the session first if not continue
@@ -49,6 +55,11 @@ class fanTurnOn(object):
         hermes.publish_start_session_notification(intent_message.site_id, "Fan Turned On", "")
 
     # More callback function goes here...
+    def lightTurnOn_callback(self, hermes, intent_message):
+        hermes.publish_end_session(intent_message.session_id, "")
+        print '[Received] intent : {}'.format(intent_message.intent.intent_name)
+        self.lightcommand()
+        hermes.publish_start_session_notification(intent_message.site_id, "Light Turned On", "")
 
     # --> Master callback function, triggered everytime an intent is recognized
     def master_intent_callback(self,hermes, intent_message):
@@ -59,6 +70,9 @@ class fanTurnOn(object):
         elif coming_intent == 'lordtyrion96:fanTurnOn':
             print("Testing 2")
             self.fanTurnOn_callback(hermes, intent_message)
+         elif coming_intent == 'lordtyrion96:lightTurnOn':
+            print("Testing 3")
+            self.lightTurnOn_callback(hermes, intent_message)
 
         # more callback and if condition goes here...
 
